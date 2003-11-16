0667c4057da2a17a4c76969ffbe4dd0ef3ecda39
Isaac Jurado <diptongo@gmail.com>
2003-11-16 23:48:12 +0100

    Definitivamente soy estúpido. El plimo tenía razón: bastaba con rotar sobre el plano XZ y luego sobre el plano ZY. Y yo complicándome la vida con senos y cosenos.
    Así pues el giro de cámara ya funciona bien. Ahora toca corregir los
    movimientos de desplazamiento de cámara y luego pasarle el control de giro al
    ratón.

--- interaccion.c
+++ interaccion.c
@@ -25,69 +25,93 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <math.h>
 
-float angulo_h = 0.0;
-float angulo_v = 0.0;
-float camara_x = 0.0;
-float camara_y = 0.2;
-float camara_z = 0.0;
-float direcc_x = 0.0;
-float direcc_y = 0.2;
-float direcc_z = 1.0;
+const GLfloat MOV_STEP = 0.1;
+const GLfloat ROT_STEP = 3.0;
+
+GLfloat angulo_h = 0.0; /* Ángulo de rotación horizontal */
+GLfloat angulo_v = 0.0; /* Ángulo de rotación vertical */
+GLfloat camara_x = 0.0; /* Posición de la cámara */
+GLfloat camara_y = 0.2;
+GLfloat camara_z = 0.0;
+
+static inline void actualiza_camara(void)
+{
+	/*
+	 * Pasos:
+	 *    1. Trasladarnos al punto donde se encuentra la cámara.
+	 *    2. Girar la cámara horizontalmente (plano XZ).
+	 *    3. Girar la cámara verticalmente (plano ZY).
+	 */
+	glLoadIdentity();
+	glRotatef(angulo_v, 1.0, 0.0, 0.0); /* 3 */
+	glRotatef(angulo_h, 0.0, 1.0, 0.0); /* 2 */
+	glTranslatef(-camara_x, -camara_y, -camara_z); /* 1 */
+	glutPostRedisplay();
+}
 
 void teclado_especial(int tecla, int x, int y)
 {
 	switch (tecla) {
-		case GLUT_KEY_LEFT:
-			// Girar izquierda
-			angulo_h -= 0.2;
-			break;
 		case GLUT_KEY_RIGHT:
-			// Girar derecha
-			angulo_h += 0.2;
+			/* Paso lateral a la derecha (strafe right)  */
+			camara_x += MOV_STEP;
+			break;
+		case GLUT_KEY_LEFT:
+			/* Paso lateral a la izquierda (strafe left) */
+			camara_x -= MOV_STEP;
 			break;
 		case GLUT_KEY_UP:
-			// Avanzar
-			camara_x += sinf(angulo_h);
-			camara_z += cosf(angulo_h);
+			/* Avanzar (FIXME) */
+			camara_z += MOV_STEP;
 			break;
 		case GLUT_KEY_DOWN:
-			// Retroceder
-			camara_x -= sinf(angulo_h);;
-			camara_z -= cosf(angulo_h);
+			/* Retroceder (FIXME) */
+			camara_z -= MOV_STEP;
 			break;
 		case GLUT_KEY_F1:
 			camara_x = 0.0;
-			camara_y = 0.0;
-			direcc_y = 0.0;
+			camara_y = 0.2;
 			camara_z = 0.0;
 			angulo_h = 0.0;
+			angulo_v = 0.0;
 	}
-	direcc_x = camara_x + sinf(angulo_h);
-	direcc_z = camara_z + cosf(angulo_h);
-	glLoadIdentity();
-	gluLookAt(camara_x, camara_y, camara_z, direcc_x, direcc_y, direcc_z,
-			0.0, 1.0, 0.0);
-	glutPostRedisplay();
+	actualiza_camara();
 }
 
 void teclado(unsigned char tecla, int x, int y)
 {
 	switch (tecla) {
 		case 'a':
-			camara_y += 0.1;
-			direcc_y += 0.1;
+			/* Elevación */
+			camara_y += MOV_STEP;
 			break;
 		case 'z':
-			camara_y -= 0.1;
-			direcc_y -= 0.1;
+			/* Descenso */
+			camara_y -= MOV_STEP;
+			break;
+		case 'd':
+			angulo_h -= 2*ROT_STEP;
+			/* NO BREAK */
+		case 'g':
+			angulo_h += ROT_STEP;
+			if (angulo_h < 0.0)
+				angulo_h += 360.0;
+			else if (angulo_h > 360.0)
+				angulo_h -= 360.0;
+			break;
+		case 'f':
+			angulo_v -= 2*ROT_STEP;
+			/* NO BREAK */
+		case 'r':
+			angulo_v += ROT_STEP;
+			if (angulo_v < 0.0)
+				angulo_v += 360.0;
+			else if (angulo_v > 360.0)
+				angulo_v -= 360.0;
 			break;
 	}
-	glLoadIdentity();
-	gluLookAt(camara_x, camara_y, camara_z, direcc_x, direcc_y, direcc_z,
-			0.0, 1.0, 0.0);
-	glutPostRedisplay();
+	actualiza_camara();
 }
 
 void redimensionado(int ancho, int alto)
