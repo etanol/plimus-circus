4e5651a43e003a42adf742dd0d1e9a9eaaa3264f
Isaac Jurado <diptongo@gmail.com>
2003-12-17 01:56:14 +0100

    Añadido el prototipo para la función grada_lateral()

--- piezas.h
+++ piezas.h
@@ -32,6 +32,9 @@ extern void faldon_lateral(float radio, float altura, int num_caras);
 extern void techo_frontal(float radio, float altura, float longitud);
 extern void techo_lateral(float radio, float altura, int num_caras);
 
-extern void grada_frontal(float altura, float longitud, float profunidad, int num_esc);
+extern void grada_frontal(float altura, float longitud, float profunidad, int
+		num_esc);
+extern void grada_lateral(float radio_in, float radio_ex, float altura, int
+		num_esc, int num_caras);
 
 #endif /* _PIEZAS_H_H */
