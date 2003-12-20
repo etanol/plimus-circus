4cd91b79758b58277b9b020b097f2a42b4a5c30a
Isaac Jurado <diptongo@gmail.com>
2003-12-20 20:48:05 +0100

    Todos los cambios de TESTING metidos en la rama principal.

--- piezas.h
+++ piezas.h
@@ -26,15 +26,24 @@
 #ifndef _PIEZAS_H_H
 #define _PIEZAS_H_H
 
-extern void faldon_frontal(float altura, float longitud);
-extern void faldon_lateral(float radio, float altura, int num_caras, int exclude);
+#define X 0
+#define Y 1
+#define Z 2
 
-extern void techo_frontal(float radio, float altura, float longitud);
-extern void techo_lateral(float radio, float altura, int num_caras);
+/*
+ * carpa.c
+ */
+extern int crear_faldon_frontal(float altura, float longitud);
+extern int crear_faldon_lateral(float radio, float altura, int num_caras);
+extern int crear_techo_frontal(float radio, float altura, float longitud);
+extern int crear_techo_lateral(float radio, float altura, int num_caras);
 
-extern void grada_frontal(float altura, float longitud, float profunidad, int
-		num_esc);
-extern void grada_lateral(float radio_in, float radio_ex, float altura, int
-		num_esc, int num_caras, int exclude);
+/*
+ * gradas.c
+ */
+extern int crear_grada_frontal(float altura, float longitud, float profundidad,
+		int num_esc);
+extern int crear_grada_lateral(float radio_in, float radio_ex, float altura,
+		int num_esc, int num_caras, int omitir);
 
-#endif /* _PIEZAS_H_H */
+#endif
