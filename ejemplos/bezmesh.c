6f1606f9fc98a01e66be0b783e8e7855096c6134
Isaac Jurado <diptongo@gmail.com>
2004-01-05 03:45:13 +0100

    Añadida interacción idéntica a "test_pieza" para poder observar mejor la perfección de la superficie. Pulsando espacio podemos ver/ocultar los puntos de control.

--- ejemplos/bezmesh.c
+++ ejemplos/bezmesh.c
@@ -22,13 +22,16 @@ GLfloat ctrlpoints[4][4][3] = {
 	 {0.5, 1.5, 0.0},   {1.5, 1.5, -1.0}}
 };
 
+float r_horizontal = 0.0;
+float r_vertical   = 0.0;
+int   dopoints = 0;
 
 void initlights(void)
 {
 	GLfloat ambient[] = {0.2, 0.2, 0.2, 1.0};
 	GLfloat position[] = {0.0, 0.0, 2.0, 1.0};
-	GLfloat mat_diffuse[] = {0.6, 0.6, 0.6, 1.0};
-	GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat mat_diffuse[] = {0.8, 0.2, 1.0, 1.0};
+	GLfloat mat_specular[] = {1.0, 0.6, 1.0, 1.0};
 	GLfloat mat_shininess[] = {50.0};
 
 	glEnable(GL_LIGHTING);
@@ -43,7 +46,7 @@ void initlights(void)
 void init(void)
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_FLAT);
+	glShadeModel(GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, ctrlpoints[0][0]);
 	glEnable(GL_MAP2_VERTEX_3);
@@ -57,25 +60,76 @@ void init(void)
 	initlights();
 }
 
+void rotar(int tecla, int x, int y)
+{
+	switch (tecla) {
+		case GLUT_KEY_LEFT:
+			r_horizontal -= 3.0;
+			break;
+		case GLUT_KEY_RIGHT:
+			r_horizontal += 3.0;
+			break;
+		case GLUT_KEY_DOWN:
+			r_vertical -= 3.0;
+			break;
+		case GLUT_KEY_UP:
+			r_vertical += 3.0;
+			break;
+	}
+	glutPostRedisplay();
+}
+
+void teclado(unsigned char tecla, int x, int y)
+{
+	switch (tecla) {
+		case '0':
+			r_horizontal = 0.0;
+			r_vertical   = 0.0;
+			break;
+		case ' ':
+			dopoints = !dopoints;
+			break;
+		case 27:
+			exit(0);
+	}
+	glutPostRedisplay();
+}
+
 void display(void)
 {
+	int i, j;
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glPushMatrix();
-	glRotatef(85.0, 1.0, 1.0, 1.0);
+	glLoadIdentity();
+	glRotatef(r_horizontal, 0.0, 1.0, 0.0);
+	glRotatef(r_vertical, 1.0, 0.0, 0.0);
 	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
-	glPopMatrix();
+	if (dopoints) {
+		glPointSize(5.0);
+		glColor3f(1.0, 1.0, 0.0);
+		glDisable(GL_LIGHTING);
+		glBegin(GL_POINTS);
+		for (i = 0; i < 4; ++i) 
+			for (j = 0; j < 4; ++j) 
+				glVertex3fv(ctrlpoints[i][j]);
+		glEnd();
+		glEnable(GL_LIGHTING);
+	}
 	glFlush();
+	glutSwapBuffers();
 }
 
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow(argv[0]);
 	init();
 	glutDisplayFunc(display);
+	glutKeyboardFunc(teclado);
+	glutSpecialFunc(rotar);
 	glutMainLoop();
 	return 0;
 }
